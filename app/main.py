9b17822625983e3b7a172ac80cec6fa3eea3903a
Dev-14112002 <gulatisaksham2002@gmail.com>
2026-05-15 19:52:28 +0530

    Implemented end-to-end RAG pipeline with FastAPI and ChromaDB

--- app/main.py
+++ app/main.py
@@ -1,7 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
+
+from app.ingest import load_and_split_pdf
+from app.rag import create_vector_store
+from pydantic import BaseModel
+from app.rag import create_qa_chain
 
 app = FastAPI()
+
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+class QueryRequest(BaseModel):
+    question: str
+
 
 @app.get("/")
 def home():
     return {"message": "AI Research Assistant Running"}
+
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+
+    file_path = f"{UPLOAD_DIR}/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    chunks = load_and_split_pdf(file_path)
+
+    create_vector_store(chunks)
+
+    return {
+        "message": f"{file.filename} uploaded successfully",
+        "chunks_created": len(chunks),
+    }
+
+
+@app.post("/query")
+def query_docs(request: QueryRequest):
+
+    qa_chain = create_qa_chain()
+
+    response = qa_chain.invoke({"query": request.question})
+
+    return {"answer": response["result"]}
---
+++ app/rag.py
@@ -0,0 +1,42 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding_model = OpenAIEmbeddings()
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+DB_PATH = "./chroma_db"
+
+
+def load_vector_store():
+
+    vector_store = Chroma(persist_directory=DB_PATH, embedding_function=embedding_model)
+
+    return vector_store
+
+
+def create_qa_chain():
+
+    vector_store = load_vector_store()
+
+    retriever = vector_store.as_retriever(search_kwargs={"k": 4})
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm, retriever=retriever, return_source_documents=True
+    )
+
+    return qa_chain
+
+
+def create_vector_store(chunks):
+
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding_model, persist_directory=DB_PATH
+    )
+
+    return vector_store
